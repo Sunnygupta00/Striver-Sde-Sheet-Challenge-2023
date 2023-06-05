a85a2336caae0b8805f522aef3685d01f4277a56
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-06-06 02:57:31 +0530

    Update 1_SetMatrixZero.java

--- DAY-1/1_SetMatrixZero.java
+++ DAY-1/1_SetMatrixZero.java
@@ -60,3 +60,49 @@ class Solution {
     }
 }
 
+// optimised approach using matrix as a maping
+// time : O(n*m) + O(n*m) + O(n) + O(m)
+// space : O(1)
+class Solution {
+    public void setZeroes(int[][] matrix) {
+     int n = matrix.length;
+     int m = matrix[0].length;
+     int col = 1;
+     for(int i=0;i<n;i++){
+         for(int j = 0;j<m;j++){
+             if(matrix[i][j] == 0){
+                 matrix[i][0] = 0;
+                 if(j !=0 ){
+                     matrix[0][j] = 0;
+                 }else{
+                     col = 0;
+                 }
+             }
+         }
+     }
+     for(int i= 1;i<n;i++){
+         for(int j = 1;j<m;j++){
+             if(matrix[0][j]==0 || matrix[i][0] == 0){
+                 matrix[i][j] = 0;
+             }
+         }
+     }
+     if(matrix[0][0] == 0){
+         fillRow(matrix, 0, m);
+     }
+     if(col == 0){
+         fillCol(matrix, n, 0);
+     }
+    }
+    public void fillCol(int arr[][], int row, int col){
+        for(int i = 0;i<row;i++){
+            arr[i][col] = 0;
+        }
+    }
+     public void fillRow(int arr[][], int row, int col){
+        for(int i = 0;i<col;i++){
+            arr[row][i] = 0;
+        }
+    }
+}
+
