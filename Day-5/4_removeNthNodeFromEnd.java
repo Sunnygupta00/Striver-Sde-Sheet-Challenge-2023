cc00bbf6ae5ba80022a75f1c607d1a108f709e82
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-06-13 00:18:30 +0530

    Update 4_removeNthNodeFromEnd.java

--- Day-5/4_removeNthNodeFromEnd.java
+++ Day-5/4_removeNthNodeFromEnd.java
@@ -18,3 +18,23 @@ class Solution {
         return head;        
     }
 }
+// Single Iterations
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null)return head;
+        ListNode dummy = new ListNode (-1);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        for(int i = 0;i<=n;i++){
+            fast = fast.next;
+        }
+        while(fast!=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;       
+    }
+}
