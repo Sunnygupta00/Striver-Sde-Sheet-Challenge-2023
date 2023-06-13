1c591fe6c2d00644fe0e6675aa841fa4723824ef
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-06-14 01:52:31 +0530

    Update 3_ReverseKGroup.java

--- Day-6/3_ReverseKGroup.java
+++ Day-6/3_ReverseKGroup.java
@@ -23,3 +23,41 @@ class Solution {
         return prev;
     }
 }
+// without space 
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || head.next == null || k == 1)return head;
+        ListNode dummy = new ListNode (-1);
+        dummy.next = head;
+        ListNode end = head;
+        ListNode prevHead = dummy;
+        int i =1;
+        while(end !=null){
+            if(i%k == 0){
+                ListNode start = prevHead.next;
+                ListNode nextStart = end.next;
+                reverse(start, end);
+                start.next = nextStart;
+                prevHead.next = end;
+                prevHead = start;
+                end = nextStart;
+                
+            }else{
+                end = end.next;
+            }
+             i++;
+        }
+        return dummy.next;
+        
+    }
+    public  void reverse(ListNode start, ListNode end){
+        ListNode prev = null;
+        ListNode curr = start;
+        while(start!=null && prev!=end){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+    }
+}
