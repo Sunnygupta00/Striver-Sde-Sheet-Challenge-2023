0447046572174f569e97bad1e5cdaef8419cbb39
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-06-15 00:07:42 +0530

    Update 6_Flattening of a LinkedList.java

--- Day-6/6_Flattening of a LinkedList.java
+++ Day-6/6_Flattening of a LinkedList.java
@@ -20,3 +20,15 @@ class Solution {
     }
 }
 //optimised
+
+class Solution {
+    private TreeNode prev = null;
+    public void flatten(TreeNode root) {
+      if(root == null)return;
+       flatten(root.right);
+       flatten(root.left);
+       root.right = prev;
+       root.left = null;
+       prev = root;
+    }
+}
