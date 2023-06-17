9d2431f80562d6b36b29266dae1243e380ba7a2d
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-06-17 23:43:45 +0530

    Update 2_Clone a Linked List with random and next pointer.java

--- Day-7/2_Clone a Linked List with random and next pointer.java
+++ Day-7/2_Clone a Linked List with random and next pointer.java
@@ -16,3 +16,44 @@ class Solution {
     }
 }
 // Optimised we will insert the node in between
+
+
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        
+       //Base case 
+        if(head==null) return head;
+//---------------------------------------------------------------------------------
+      // Inserting the the clone in b/w so that we can easily copy original
+        for(Node curr=head; curr!=null; curr=curr.next.next){
+   
+            Node temp=curr.next; // holding curr next
+            Node clone=new Node (curr.val);// created clone;
+            curr.next=clone;// adding clone
+            clone.next=temp;//conecting with original
+   
+        }
+//---------------------------------------------------------------------------------        
+        
+        // NOW JUST COPYING THE RANDOM POINTERS
+        for(Node curr=head; curr!=null; curr=curr.next.next){
+            if(curr.random!=null){
+                curr.next.random=curr.random.next;
+            }
+        }  
+//---------------------------------------------------------------------------------   
+        // NOW WE ARE SEPRATING BOTH ORGINAL AND CLONE ONE
+        Node dummy =new Node(-1);// we will attack the clone one with this
+        Node temp=dummy;//pointer for dummy
+        
+        for(Node curr=head; curr!=null; curr=curr.next){
+            temp.next=curr.next;// CONNECTING TEMP WITH CLONE ONE
+            temp=temp.next;    // NOW MOVING AHEAD POINTER
+            curr.next=temp.next;// ALSO CONNECTIN THE ORIGINAL POINTER
+        }
+        
+//---------------------------------------------------------------------------------
+        return dummy.next;// AS DUMMY IS -1 WE WILL RETURN DUMMY.NEXT
+    }
+}  
