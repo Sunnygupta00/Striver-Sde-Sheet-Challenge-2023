c1c226e46c1a07106d5b095466f0000091c3e9dd
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-06-19 02:42:57 +0530

    Create 3_combinationSum.java

---
+++ Day-9 | Recursion/3_combinationSum.java
@@ -0,0 +1,21 @@
+// Simple pick and not pick with little twist
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        rec(arr, ans, new ArrayList<>(), target, arr.length, 0);
+        return ans;
+    }
+    public void rec(int arr[], List<List<Integer>> ans,List<Integer>temp,  int target, int n, int index){
+        if(target <=0  || index == n){
+            if(target == 0){
+                ans.add(new ArrayList<>(temp));
+            }
+            return;
+        }
+        temp.add(arr[index]);
+        rec(arr, ans, temp, target-arr[index],n, index+1);
+        temp.remove(temp.size()-1);
+        rec(arr, ans, temp, target, n, index+1);
+    }
+}
