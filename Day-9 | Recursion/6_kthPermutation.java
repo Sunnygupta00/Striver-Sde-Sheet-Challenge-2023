bfde98f6c096b79172ca1fef2c5092f5131e46f7
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-06-19 23:20:47 +0530

    Create 6_kthPermutation.java

---
+++ Day-9 | Recursion/6_kthPermutation.java
@@ -0,0 +1,29 @@
+// brute force generate all permutaion and then then simply return kth index
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<String> per = new ArrayList<>();
+        char arr[] = new char[n];
+        for(int i=0;i<n;i++){
+            arr[i] = (char)('1'+i);
+        }
+        compute(arr, 0, per);
+        Collections.sort(per);
+        return per.get(k-1);
+    }
+    public void compute(char arr[], int index, List<String>per){
+        if(arr.length == index){
+            per.add(new String(arr));
+            return;
+        }
+        for(int i=index;i<arr.length;i++){
+            swap(arr, index, i);
+            compute(arr, index+1, per);
+            swap(arr, index, i);
+        }
+    }
+    public void swap(char arr[], int i, int j){
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
