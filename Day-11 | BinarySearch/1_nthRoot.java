3ca2d5f71c5476f6ee6c9b401bb59cd90bb2d12a
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-06-25 22:33:39 +0530

    Update 1_nthRoot.java

--- Day-11 | BinarySearch/1_nthRoot.java
+++ Day-11 | BinarySearch/1_nthRoot.java
@@ -3,18 +3,28 @@ public class Solution {
         // Write your code here.
         int start = 1;
         int end = m;
-        while(start<=end){
+        while(start <= end){
             int mid = start+(end-start)/2;
-            long realMid = (long)Math.pow(mid,n);
-            if(realMid == m){
+            long res = nextMove(mid, n, m);  // intead of this we can also use Math.pow(x,n);
+            if( res == 0){
                 return mid;
-            }else if(realMid < m){
-                start = mid+1;
-            }else if(realMid > m){
-                end = mid-1;
+            }else if(res == 1){
+               start = mid+1;
+            }else{
+               end= mid-1;
             }
-        }
+        }        
         return -1;
     }
-   
+    public static int nextMove(int n, int pow, int m){
+        long ans = 1;
+        for(int i=0;i<pow;i++){
+            ans*=n;
+            if(ans >m){ // if it exeeds
+                return 2;
+            }
+        }
+        if(ans < m)return 1;// if it is less
+        return 0; // if it is equal
+    }
 }
